4e8ac544e1680b7f8b3f8bb2922ee2d8fdf4ad4e
Abdur-Rahman-Argon <abdurrahman.me7@gmail.com>
2022-12-06 21:05:05 +0600

    Reducer sample

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import "./App.css";
-import { createContext, useState } from "react";
-import ParentsComponents from "./components/ParentsComponents/ParentsComponents";
+import { createContext, useReducer, useState } from "react";
+// import ParentsComponents from "./components/ParentsComponents/ParentsComponents";
 
 export const Counter_Context = createContext();
 
 function App() {
-  const [number, setNumber] = useState(0);
+  const initialState = 0;
 
-  const incrise = () => {
-    setNumber(number + 1);
+  const reducer = (state, action) => {
+    if (action.type === "INCREMENT") {
+      return state + 1;
+    } else if (action.type === "DECREMENT") {
+      return state - 1;
+    }
   };
 
-  const decrise = () => {
-    setNumber(number - 1);
-  };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const value = { incrise, decrise, number, setNumber };
+  // const [number, setNumber] = useState(0);
+
+  // const incrise = () => {
+  //   setNumber(number + 1);
+  // };
+
+  // const decrise = () => {
+  //   setNumber(number - 1);
+  // };
+
+  // const value = { incrise, decrise, number, setNumber };
 
   return (
     <div className="App">
       <h1> Reducer Practice</h1>
-      <Counter_Context.Provider value={value}>
+      {/* <Counter_Context.Provider value={value}>
         <div>
           <ParentsComponents />
         </div>
-      </Counter_Context.Provider>
+      </Counter_Context.Provider> */}
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>increment</button>
+      <p>{state}</p>
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>decrement</button>
     </div>
   );
 }
